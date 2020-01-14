9fb5d9015f0778994f2af633dcb426765659397d
13664505637 <13664505637@163.com>
2020-01-14 11:32:58 +0800

    根据parentId获取相应字典表集合

--- src/main/java/com/ygjy/service/DictionaryTableService.java
+++ src/main/java/com/ygjy/service/DictionaryTableService.java
@@ -3,11 +3,24 @@ package com.ygjy.service;
 import com.ygjy.entity.DictionaryTable;
 
 import java.util.List;
-
+/**
+ * @Author yuyang
+ * @Description //字典表service接口
+ * @Date 11:09 2020/1/14
+ **/
 public interface DictionaryTableService {
     //查询居住方式
     List<DictionaryTable> livingType();
 
     //查询居住事由
     List<DictionaryTable> liveFor();
+
+    /**
+     * @Author yuyang
+     * @Description //根据parentId获取字典表集合
+     * @Date 11:08 2020/1/14
+     * @param parentId
+     * @return java.util.List<com.ygjy.entity.DictionaryTable>
+     **/
+    List<DictionaryTable> getDictionaryTableList(String parentId);
 }
